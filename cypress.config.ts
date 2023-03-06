15c50a8db8da7df6640c41845decf5ba499679bb
bencergazda <level@bencergazda.hu>
2023-03-06 14:26:59 +0100

    Use webpack config from `react-app-rewired` (working)
    As how returned in `@cypress/webpack-dev-server/dist/helpers/createReactAppHandler.js:67`
    
    Everything works well.

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,5 +1,13 @@
 import { defineConfig } from "cypress";
 
+/**
+ * "Rewire" the webpack config before `loadWebpackConfig` accesses it through `react-scripts`
+ * @see @cypress/webpack-dev-server/dist/helpers/createReactAppHandler.js:41
+ */
+process.env.NODE_ENV = "test";
+process.env.BABEL_ENV = "test";
+require("react-app-rewired/config/webpack.config")("development");
+
 export default defineConfig({
   component: {
     devServer: {
