6fc56604e79851ac7bdd9a979ab2e79634ebb2fc
bencergazda <level@bencergazda.hu>
2023-03-06 15:29:05 +0100

    Use webpack config from `react-scripts` through `devServerOptions` (not working)
    As how returned in `cypress.config.ts:16`
    
    Webpack fails to start with the following error:
    ```
    ValidationError: Invalid configuration object. Webpack has been initialized using a configuration object that does not match the API schema.
     - configuration.output.devtoolModuleFilenameTemplate should be one of these:
       string | function
       -> Filename template string of function for the sources array in a generated SourceMap.
     ```

--- cypress.config.ts
+++ cypress.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
           process.env.NODE_ENV = "test";
           process.env.BABEL_ENV = "test";
 
-          const webpackConfig = require("react-app-rewired/config/webpack.config")("test");
+          const webpackConfig = require("react-scripts/config/webpack.config")("test");
 
           return webpackConfig;
         },
