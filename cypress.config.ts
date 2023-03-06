0ba9cfc29a9e3330386bff68d57e223380cc7375
bencergazda <level@bencergazda.hu>
2023-03-06 15:11:15 +0100

    Use webpack config from `react-app-rewired` through `devServerOptions` (not working)
    As how returned in `cypress.config.ts:16`
    
    Webpack can start, but throws the following error:
    
    ```
    ERROR in ./cypress/support/component.ts 30:8
    Module parse failed: Unexpected token (30:8)
    File was processed with these loaders:
     * ./node_modules/@pmmmwh/react-refresh-webpack-plugin/loader/index.js
     * ./node_modules/source-map-loader/dist/cjs.js
    You may need an additional loader to handle the result of these loaders.
    ```

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,18 +1,20 @@
 import { defineConfig } from "cypress";
-
-/**
- * "Rewire" the webpack config before `loadWebpackConfig` accesses it through `react-scripts`
- * @see @cypress/webpack-dev-server/dist/helpers/createReactAppHandler.js:41
- */
-process.env.NODE_ENV = "test";
-process.env.BABEL_ENV = "test";
-require("react-app-rewired/config/webpack.config")("development");
+import { devServer } from "@cypress/webpack-dev-server";
 
 export default defineConfig({
   component: {
-    devServer: {
-      framework: "create-react-app",
-      bundler: "webpack",
-    },
+    devServer: devServerOptions =>
+      devServer({
+        ...devServerOptions,
+        framework: "react",
+        webpackConfig: () => {
+          process.env.NODE_ENV = "development";
+          process.env.BABEL_ENV = "development";
+
+          const webpackConfig = require("react-app-rewired/config/webpack.config")("development");
+
+          return webpackConfig;
+        },
+      }),
   },
 });
